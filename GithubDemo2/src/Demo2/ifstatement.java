016a5217b1c553d64e8f469a5380de0724627362
GithubTesting112 <165751707+GithubTesting112@users.noreply.github.com>
2024-04-02 16:53:53 +0530

    Initial Commit

---
+++ GithubDemo2/src/Demo2/ifstatement.java
@@ -0,0 +1,19 @@
+package Demo2;
+
+import java.util.Scanner;
+
+public class ifstatement {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+        scanner.close();
+    }
+}
